2c3271ec264e1ef75e10da8b609268169e7f0781
SuchitaKhadse <suchitakhadse@15gmail.com~>
2023-04-14 22:29:08 +0530

    ModifiedPush

--- src/test/java/com/carwale23a/CarTests.java
+++ src/test/java/com/carwale23a/CarTests.java
@@ -1,6 +1,8 @@
 package com.carwale23a;
+
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;import org.openqa.selenium.WebElement;
+import java.lang.reflect.InvocationTargetException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -12,39 +14,41 @@ import com.carwale23a.pages.NewCarsPage;
 import com.carwale23a.util.Environment;
 
 public class CarTests extends TestBase {
-UIKeyword keyword = new UIKeyword();
+	UIKeyword keyword = new UIKeyword();
 
 	@Test
-	public void verifySearchResultForMarutiBrand() throws IOException, InvocationTargetException, InterruptedException{
+	public void verifySearchResultForMarutiBrand() throws IOException, InvocationTargetException, InterruptedException {
 		UIKeyword.launchUrl(Environment.URL);
 		Homepage home = PageFactory.initElements(UIKeyword.driver, Homepage.class);
 		home.hoverOnNewCar();
 		home.clickOnfindNewCars();
-	
-         UIKeyword.scrollby();
-            
-		NewCarsPage newcarpage=PageFactory.initElements(UIKeyword.driver, NewCarsPage.class);
+
+		UIKeyword.scrollby();
+
+		NewCarsPage newcarpage = PageFactory.initElements(UIKeyword.driver, NewCarsPage.class);
 		UIKeyword.waitForElementToBePresent(newcarpage.marutiSuzuki);
 		newcarpage.clickOnMarutiSuzuki();
-		
+
 		MarutiCarPage maruticarpage = PageFactory.initElements(UIKeyword.driver, MarutiCarPage.class);
 		maruticarpage.getTextFromMarutiSuzukiBrezza();
-		
-		
-		
+
+	}
+	@Test
+	public void verifySearchResultForTataBrand() {
+	
+		UIKeyword.launchUrl(Environment.URL);
+		Homepage home = PageFactory.initElements(UIKeyword.driver, Homepage.class);
+		home.hoverOnNewCar();
+		home.clickOnfindNewCars();
+
+		UIKeyword.scrollby();
+		NewCarsPage newcarpage = PageFactory.initElements(UIKeyword.driver, NewCarsPage.class);
+		UIKeyword.waitForElementToBePresent(newcarpage.Tata);
+		newcarpage.clickOntata();
+		MarutiCarPage maruticarpage = PageFactory.initElements(UIKeyword.driver, MarutiCarPage.class);
+		maruticarpage.getTextFromTata();
+
+
 	}
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 
