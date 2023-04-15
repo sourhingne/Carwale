a02aaac6bb98cb60ce728634e25ce46797ff8bd8
Dhanushivansh <deshpandedhananjay84@gmail.com>
2023-04-15 15:48:17 +0530

    mynewtestcase

--- src/main/java/com/carwale23a/pages/NewCarsPage.java
+++ src/main/java/com/carwale23a/pages/NewCarsPage.java
@@ -7,22 +7,50 @@ import com.carwale23a.config.TestBase;
 import com.carwale23a.keywords.UIKeyword;
 
 public class NewCarsPage extends TestBase {
-	@FindBy(css="  a > div.o-cohPRA.o-cohQay.UaZtoi > div > img")
-     public WebElement marutiSuzuki;
-	@FindBy(css="ul > li.o-fzoHAP > div > div > div > a > h3")
+	@FindBy(css = "  a > div.o-cohPRA.o-cohQay.UaZtoi > div > img")
+	public WebElement marutiSuzuki;
+
+	@FindBy(css = "ul > li.o-fzoHAP > div > div > div > a > h3")
 	public WebElement Tata;
+
+	@FindBy(css = " div > ul > li:nth-child(5) > a > div.o-cpNAVm.o-byFsZJ.o-eemiLE.o-fzpihx")
+	public WebElement Toyota;
+
+	@FindBy(css = " div > ul > li:nth-child(6) a > div.o-cohPRA.o-cohQay.UaZtoi > div > img")
+	public WebElement Kia;
+
 	public void clickOnMarutiSuzuki() {
-          UIKeyword.clickOn(marutiSuzuki);
+		UIKeyword.clickOn(marutiSuzuki);
 	}
-	public  void hoverOnMarutiSuzuki() {
-        UIKeyword.mousemove(marutiSuzuki);
+
+	public void hoverOnMarutiSuzuki() {
+		UIKeyword.mousemove(marutiSuzuki);
 	}
+
 	public void clickOntata() {
-        UIKeyword.clickOn(Tata);
+		UIKeyword.clickOn(Tata);
 	}
-	public  void hoverOntata() {
-      UIKeyword.mousemove(Tata);
+
+	public void hoverOntata() {
+		UIKeyword.mousemove(Tata);
 	}
-	
-	
+
+	public void clickOnToyota() {
+		UIKeyword.clickOn(Toyota);
+	}
+
+	public void hoverOnToyota() {
+		UIKeyword.mousemove(Toyota);
+
+	}
+
+	public void clickOnKia() {
+		UIKeyword.clickOn(Kia);
+	}
+
+	public void hoverOnKia() {
+		UIKeyword.mousemove(Kia);
+
+	}
+
 }
